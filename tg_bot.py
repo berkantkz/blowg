71a79366e3cfe49dd93910ccbc92f87ac4f85d6c
berkant <berkantkorkmaz@outlook.com>
2022-06-25 06:13:26 +0300

    bot: complete

--- tg_bot.py
+++ tg_bot.py
@@ -10,9 +10,24 @@
 import requests
 import os
 
-from yarl import URL
+feed = "https://berkantkz.github.io/feed.json"
+spotify = "https://open.spotify.com/track/"
 
 berkantkzBot = os.getenv('berkantkzBot')
-url = "https://api.telegram.org/bot" + berkantkzBot + "/sendMessage?chat_id=@u_berkantkz&parse_mode=markdown&text="
 
-#requests.post(url)
+post = requests.get(feed).json()
+
+message = ''
+
+if post[0]['posted'] == None :
+    print("posting to telegram")
+    message += '\n[' + post[0]['name'] + '](' + post[0]['url'] + ') - '
+    message += '__' + post[0]['date'] + '\n__'
+    if post[0]['song'] :
+        message += "\n[spotify](" + spotify + post[0]['song'] + ")\n"
+    message += "\n"
+    message += post[0]['content']
+
+url = "https://api.telegram.org/bot" + berkantkzBot + "/sendMessage?chat_id=@u_berkantkz&parse_mode=markdown&text=" + message
+
+requests.post(url)
